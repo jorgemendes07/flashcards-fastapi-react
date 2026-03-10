9ba3231ee2470dde4c171cf76c498e35706a98ca
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-03-09 21:55:01 -0300

    feat: cria o model user.py

---
+++ backend/app/models/user.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+
+from app.database.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+    decks: Mapped[List["Deck"]] = relationship(back_populates="user", cascade="all, delete-orphan")

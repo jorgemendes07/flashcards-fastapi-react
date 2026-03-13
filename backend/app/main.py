ac4d91d3d9756ef4b89886ca1d8b05cf3ebd3b0e
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-03-12 23:08:39 -0300

    feat: implementa lifespan para inicalizacao do banco de dados

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.database.database import engine, Base
+import app.models
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+    pass
+
+app = FastAPI(
+    title="Flashcards App",
+    lifespan=lifespan
+)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+    return {"Message": "Flashcards App"}

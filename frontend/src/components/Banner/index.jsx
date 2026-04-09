654ef819a884c510a392401e2f49f102b5f67d3d
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-04-09 20:26:21 -0300

    feat: cria o estilo dos decks

--- frontend/src/components/Banner/index.jsx
+++ frontend/src/components/Banner/index.jsx
@@ -2,10 +2,10 @@ export default function Banner(props) {
 
     return (
         <div className="border-b-2 border-gray-200 flex items-center place-content-between px-6 py-4">
-            <h1 className="text-3xl">Flashcards App</h1>
+            <h1 className="text-xl">Flashcards App</h1>
             <button 
                 onClick={props.onOpenModal}
-                className="bg-orange-300 px-3 py-1 rounded-lg cursor-pointer hover:bg-orange-400"
+                className="bg-orange-300 text-sm px-3 py-1 rounded-lg cursor-pointer hover:bg-orange-400"
             >
                 + Novo Deck
             </button>
--- frontend/src/components/DeckList/index.jsx
+++ frontend/src/components/DeckList/index.jsx
@@ -1,12 +1,33 @@
+import deckIcon from './../../assets/deck-icon.png'
+
 export default function({ decks }) {
     
     return (
-        <div>
-            <ul>
+        <div className="bg-gray-100 h-screen p-4 m-auto">
+            <div>
+                <h2 className='text-xl'>Seus Decks</h2>
+                <p className='text-sm'>Selecione um deck para começar a práticar</p>
+            </div>
+            <div>
                 {decks.map((deck) => (
-                    <li key={deck.id}>{deck.name}</li>
+                    <div className="flex rounded-md h-18 w-[95%] h-18 m-auto mt-3 p-3 bg-white text-gray-600 cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-md">
+                        <img src={deckIcon} alt="Icone de cartas de baralho"
+                            className='w-8 h-8 rounded-lg mr-4'
+                        />
+                        <div className='flex flex-1 justify-between'>
+                            <div 
+                                key={deck.id}
+                            >
+                                {deck.name}
+                            </div>
+                            <div className='text-sm text-gray-400'>
+                                <i className="fa-solid fa-pen cursor-pointer mr-1 hover:text-gray-500"></i>
+                                <i className="fa-solid fa-trash cursor-pointer hover:text-gray-500"></i>
+                            </div>
+                        </div> 
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     )
 }
